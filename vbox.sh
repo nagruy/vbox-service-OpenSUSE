aca31e25083cc878f01db85cfe38bd48d975aaa3
nagruy <uy.nagr@gmail.com>
2018-04-30 22:02:14 -0300

    working for every user in vboxusers

--- vbox.sh
+++ vbox.sh
@@ -1,40 +1,74 @@
 #!/bin/bash
 #
-[ -r /opt/vbox/vms ] && . /opt/vbox/vms
+# Comando 'VBoxManage' ¿existe?
+VBOX_MANAGE=/usr/bin/VBoxManage
+if [[ ! -x $VBOX_MANAGE ]]; then
+  echo "$VBOX_MANAGE no existe"
+  exit 1;
+fi
 
-VBOXMANAGE=/usr/bin/VBoxManage
+# Comando para encender las vms.
+VBOX_HEADLESS="$VBOX_MANAGE startvm -type headless"
+
+# Usuarios que se encuentran en el grupo 'vboxusers' (agrego a root)
+VBOX_USERS=`/usr/bin/getent group vboxusers | /usr/bin/cut -d : -f 4 | /usr/bin/sed "s/,/\n/g" | /usr/bin/sed "1 i\root"`;
 
 start()
 {
-	for v in $VMS; do
-		$VBOXMANAGE startvm -type headless $v
-	done
+    # Para cada usuario del grupo vboxusers.
+    for user in $VBOX_USERS; do
+
+        # Home del usuario.
+        USER_HOME="`/usr/bin/getent passwd $user | /usr/bin/cut -d: -f 6`";
+
+        # lista de vms
+        VMS_FILE="$USER_HOME/.vms";
+
+        # ¿Existe el archivo de lista?   
+        if [ -f $VMS_FILE ] ; then
+
+            # 'Cargo' la variable VMS
+            [ -r $VMS_FILE ] && . $VMS_FILE
+
+            # La variable debe llamarse 'VMS'
+            for vm in $VMS; do
+
+                # Enciendo la vm headless.
+                /usr/bin/su - $user -c "$VBOX_HEADLESS $vm"
+
+            done                        
+        fi
+    done
 }
 
 stop()
 {
-	VMS=`$VBOXMANAGE list runningvms | sed -e 's/^".*".*{\(.*\)}/\1/' 2>/dev/null`
-	if [ -n "$VMS" ]; then
-		for v in $VMS; do
-        	$VBOXMANAGE controlvm $v savestate
+    # Para cada usuario del grupo vboxusers.
+    for user in $VBOX_USERS; do
+
+        # Lista de vms encendidas.
+        VMS_ON=`/usr/bin/sudo /usr/bin/su - $user -c "$VBOX_MANAGE list runningvms" | /usr/bin/cut -d ' ' -f 1 | /usr/bin/sed 's/\"//g'`
+
+        for vm in $VMS_ON; do
+            # Guardo estado de las vms.
+            echo "/usr/bin/su - $user -c \"$VBOX_MANAGE controlvm $vm savestate\""
         done
-	fi
+    done
 }
 
 case "$1" in
-	start)
-	    start
-	    ;;
-	stop)
-		stop
-	    ;;
-	restart)
-	    stop && start
-	    ;;
-	*)
-	    echo "Usage: $0 {start|stop|save|restart}"
-	    exit 1
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop && start
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|save|restart}"
+        exit 1
 esac
 
 exit 0
-
